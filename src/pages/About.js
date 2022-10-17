d62ff19b8814e3695713e78d619b75bd672c0326
Flavie Bourcet <flavie.bourcet@gmail.com>
2022-10-17 14:34:47 +0200

    ajout du component Banner à la page À Propos

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "../style/pages/About.css";
+import Banner from "../components/Banner";
+import bannerAboutImage from "../img/banner_about.jpg";
 
 export default function About() {
   return (
-    <div>
-      <h1>A propos</h1>
+    <div className="aboutBody">
+      <Banner img={bannerAboutImage} />
+      <section className="collapse"></section>
     </div>
   );
 }
