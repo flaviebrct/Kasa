141d41c133b89c6e1976717ee1d955078f9ca0e1
Flavie Bourcet <flavie.bourcet@gmail.com>
2022-10-17 18:08:33 +0200

    ajout d'un state pour afficher ou cacher les dropdown

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -3,18 +3,23 @@ import dropDownArrow from "../img/dropDownArrow.svg";
 import "../style/components/Collapse.css";
 
 function Collapse(props) {
+  const [collapseOpen, setCollapseOpen] = useState(null);
+  const collapseState = () => setCollapseOpen(!collapseOpen);
+
   return (
     <div className="dropDownBox">
-      <button className="dropDownBtn">
+      <button className="dropDownBtn" onClick={collapseState}>
         {props.title}
         <img
-          className="dropDownArrow"
+          className={collapseOpen ? "arrow open" : "arrow"}
           src={dropDownArrow}
           alt="fleche menu déroulant"
         ></img>
       </button>
 
-      <div className="boxContent">{props.content}</div>
+      <div className={collapseOpen ? "boxContent" : "boxContent hidden"}>
+        {props.content}
+      </div>
     </div>
   );
 }
