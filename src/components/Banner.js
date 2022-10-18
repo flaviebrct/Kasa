e3ecf34f651764d3aac5a7f2ed947f2941a157b8
Flavie Bourcet <flavie.bourcet@gmail.com>
2022-10-18 15:58:36 +0200

    js et css page erreur 404 + améliorations header

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import "../style/components/Banner.css";
 export default function Banner(props) {
   return (
     <section className="banner">
-      <h1>{props.h1}</h1>
+      <h1 className="bannerTitle">{props.h1}</h1>
       <div className="blackBaground">
         <img src={props.img} alt="banner" className="bannerImg"></img>
       </div>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "../style/pages/404.css";
+import { Link } from "react-router-dom";
 
 export default function Error() {
   return (
-    <div>
-      <h1>404</h1>
+    <div className="errorContainer">
+      <h1 className="errorTitle">404</h1>
+      <p className="errorContent">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/">
+        <p className="errorLink">Retourner sur la page d’accueil</p>
+      </Link>
     </div>
   );
 }
