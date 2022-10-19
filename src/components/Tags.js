bc49fa58c77d4ecac3d825b10649749220bace77
Flavie Bourcet <flavie.bourcet@gmail.com>
2022-10-19 11:04:13 +0200

    affichage des données selon Id + 404 si mauvais Id

---
+++ src/components/Tags.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Tags(props) {
+  return <div>{props.tags}</div>;
+}
--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useParams, Navigate } from "react-router-dom";
+import "../style/pages/Logement.css";
+import Data from "../data/data";
+import Tags from "../components/Tags";
 
 export default function Logement() {
+  const params = useParams();
+  const validProperty = Data.find((property) => property.id === params.id);
+
+  if (!validProperty) {
+    return <Navigate to="/404" />;
+  }
+
   return (
     <div>
-      <h1>Logement</h1>
+      <h1>{validProperty.title}</h1>
+      <p>{validProperty.location}</p>
+      <Tags content={validProperty.tags} />
     </div>
   );
 }
