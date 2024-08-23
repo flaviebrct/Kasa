88d1537e729cd49604d383ae191e6795bf362eff
Flavie Bourcet <flavie.bourcet@gmail.com>
2024-08-23 19:26:25 +0200

    Updating Error 404 page css to scss

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,15 @@
 import React from "react";
-import "../style/pages/404.css";
 import { Link } from "react-router-dom";
 
 export default function Error() {
   return (
     <div className="errorContainer">
-      <h1 className="errorTitle">404</h1>
-      <p className="errorContent">
+      <h1 className="errorTitle bold">404</h1>
+      <p className="errorContent thin">
         Oups! La page que vous demandez n'existe pas.
       </p>
       <Link to="/">
-        <p className="errorLink">Retourner sur la page d’accueil</p>
+        <p className="errorLink thin">Retourner sur la page d’accueil</p>
       </Link>
     </div>
   );
