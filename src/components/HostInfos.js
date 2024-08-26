14058c1b2675729236edab313b07145d3cb2c6f9
Flavie Bourcet <flavie.bourcet@gmail.com>
2024-08-26 16:04:54 +0200

    Updating Host infos component css to scss

--- src/components/HostInfos.js
+++ src/components/HostInfos.js
@@ -1,10 +1,9 @@
 import React from "react";
-import "../style/components/HostInfos.css";
 
 export default function HostInfos(props) {
   return (
     <div className="hostInfos">
-      <p>{props.name}</p>
+      <p className="hostName thin">{props.name}</p>
       <img src={props.picture} alt="Propriétaire du logement" />
     </div>
   );
