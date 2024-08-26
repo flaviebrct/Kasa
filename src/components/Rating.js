8588958cf19dfcd32d5a98fedc723db5b9f6b588
Flavie Bourcet <flavie.bourcet@gmail.com>
2024-08-26 16:10:31 +0200

    Updating Rating component css to scss

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,16 +1,15 @@
 import React from "react";
-import "../style/components/Rating.css";
 import redStar from "../img/redStar.svg";
 import greyStar from "../img/greyStar.svg";
 
 export default function Rating(props) {
   return (
     <div className="ratingContainer">
-      <img src={props.star >= 1 ? redStar : greyStar} alt="Note de ce logement." />
-      <img src={props.star >= 2 ? redStar : greyStar} alt="Note de ce logement." />
-      <img src={props.star >= 3 ? redStar : greyStar} alt="Note de ce logement." />
-      <img src={props.star >= 4 ? redStar : greyStar} alt="Note de ce logement." />
-      <img src={props.star >= 5 ? redStar : greyStar} alt="Note de ce logement." />
+      <img className="star" src={props.star >= 1 ? redStar : greyStar} alt="Note de ce logement." />
+      <img className="star" src={props.star >= 2 ? redStar : greyStar} alt="Note de ce logement." />
+      <img className="star" src={props.star >= 3 ? redStar : greyStar} alt="Note de ce logement." />
+      <img className="star" src={props.star >= 4 ? redStar : greyStar} alt="Note de ce logement." />
+      <img className="star" src={props.star >= 5 ? redStar : greyStar} alt="Note de ce logement." />
     </div>
   );
 }
